a6089e58dca849bb6aaea67e03c9d9c0dbefb9c5
YashKachaRK <ykacha243@rku.ac.in>
2025-07-09 11:36:26 +0530

    new code

--- C3.cs
+++ C3.cs
@@ -12,6 +12,17 @@ namespace CLassWork
         {
             for (int i = 1; i <= 5; i++)
             {
+
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+
+                }
+                Console.WriteLine();
+            }
+            for (int i = 4; i >=1 ; --i)
+            {
+
                 for (int j = 1; j <= i; j++)
                 {
                     Console.Write(j);
