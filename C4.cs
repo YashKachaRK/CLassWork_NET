b27608850fffb881928ca9a391b132efc6e879c6
YashKachaRK <ykacha243@rku.ac.in>
2025-07-09 11:02:06 +0530

    New File

---
+++ C4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CLassWork
+{
+    internal class C4
+    {
+        public static void Main(string[] args)
+        {
+            int a = 2;
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("a = 1");
+                    break;
+
+                case 2:
+                    Console.WriteLine("a = 2");
+                    break;
+                default:
+                    Console.WriteLine("a is not");
+                    break;
+            }
+        }
+    }
+}
